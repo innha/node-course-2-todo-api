ff0ea5f61a7fe38b650b14926f55134a3890bf5d
innha <iharelimana@gmail.com>
2017-08-28 23:17:26 +0200

    Add test for GEt /todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+// mongoose.createConnection('mongodb://localhost:27017/TodoApp');
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const port = 3000;
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {user} = require('./models/user');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -18,12 +20,22 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
+    console.log(e);
     res.status(400).send(e);
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started at port 3000');
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    console.log(e);
+    res.status(400).send(e);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started at port ${port}`);
 });
 
 module.exports = {app};
