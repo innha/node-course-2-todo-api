d31a14c59f06cde480ada38551838cd8fa6051bc
innha <iharelimana@gmail.com>
2017-08-29 13:56:15 +0200

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -79,12 +79,12 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findOneAndRemove(id).then((todo) => {
+  Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
 
-    res.status(200).send(todo);
+    res.status(200).send({todo});
   }).catch((e) => res.status(400).send());
 });
 
