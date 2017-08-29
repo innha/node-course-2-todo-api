ec6d864d1092be5dd66a838c183fe624f28c9f07
innha <iharelimana@gmail.com>
2017-08-29 11:51:48 +0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   // console.log('POST /todos');
-  
+
   var todo = new Todo({
     text: req.body.text
   });
